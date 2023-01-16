fbfe82a44e73a7aac852a528dac666d99282a05e
Randall Wood <297232+rhwood@users.noreply.github.com>
2023-01-16 06:29:16 -0500

    use exact dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["WMATAUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/emma-k-alexandra/WMATA.swift", from: "15.0.0"),
+        .package(url: "https://github.com/emma-k-alexandra/WMATA.swift", exact: "15.0.0"),
         .package(url: "https://github.com/nalexn/ViewInspector", exact: "0.9.5")
     ],
     targets: [
