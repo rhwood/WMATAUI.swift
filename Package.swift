4901d88d4ccec75c32be235939e647bb7a8bf1e4
Randall Wood <297232+rhwood@users.noreply.github.com>
2025-06-14 10:58:08 -0400

    backout any changes to ViewInspector

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // let dependency that may be used directly by incorporating app be minimum version
         .package(url: "https://github.com/emma-k-alexandra/WMATA.swift", from: "15.2.0"),
         // build environment and test dependencies are exact
-        .package(url: "https://github.com/nalexn/ViewInspector", exact: "0.10.1"),
+        .package(url: "https://github.com/nalexn/ViewInspector", exact: "0.10.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", exact: "1.4.4")
     ],
     targets: [
