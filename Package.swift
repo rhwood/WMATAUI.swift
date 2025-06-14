5fe2f4d7610991dc3464d579684843ca4c7395fb
Randall Wood <297232+rhwood@users.noreply.github.com>
2025-06-14 11:04:46 -0400

    chore: general updates
    Now that I have a MacBook that can run Xcode 16.4, make some general updates to silence a warning, more frequently check for dependency updates, and use exact dependencies.

--- Package.swift
+++ Package.swift
@@ -17,9 +17,11 @@ let package = Package(
             targets: ["WMATAUI"])
     ],
     dependencies: [
+        // dependencies that incorporating apps may directly depend upon have a minimum version
         .package(url: "https://github.com/emma-k-alexandra/WMATA.swift", from: "15.2.0"),
-        .package(url: "https://github.com/nalexn/ViewInspector", from: "0.9.11"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+        // test and documentation dependencies are absolute
+        .package(url: "https://github.com/nalexn/ViewInspector", exact: "0.9.11"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", exact: "1.3.0")
     ],
     targets: [
         .target(
